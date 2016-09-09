14e336f01698a0b29a2d28836cf0fc3fd7150adf
Muchen He <mansurhe@outlook.com>
2016-09-09 15:10:56 -0700

    Updated p5 and rockets

---
+++ p5/sketch.js
@@ -0,0 +1,20 @@
+// p5
+var range = 100;
+var variation = 10;
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+}
+
+function mouseMoved() {
+  stroke('rgba(20, 20, 50, 0.1)');
+  line(mouseX + random(-variation, variation),
+   mouseY + random(-variation, variation),
+   mouseX + random(-range, range),
+   mouseY + random(-range, range));
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
