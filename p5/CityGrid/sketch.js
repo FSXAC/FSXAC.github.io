7bdd038e3d9810736998b26f2f1f4990ef3bb6d2
Muchen He <mansurhe@outlook.com>
2017-02-28 20:12:43 -0800

    made into circuit

--- p5/CityGrid/sketch.js
+++ p5/CityGrid/sketch.js
@@ -1,4 +1,5 @@
 var p = new Array();
+var side = 100;
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
@@ -7,13 +8,30 @@ function setup() {
 
     // setup drawing
     smooth();
-    strokeWeight(1.4);
+    strokeWeight(1);
     background(0);
-    stroke(255, 0.5);
+    stroke(255)
 
     // particle test
     for (var i = 0; i < 8; i++) {
-        append(p, new Particle(createVector(width / 2, height / 2), i * 2*PI / 8));
+        append(p, new Particle(
+            createVector(width/2-side/2+i*side/8+side/16, height/2-side/2), PI
+        ));
+    }
+    for (var i = 0; i < 8; i++) {
+        append(p, new Particle(
+            createVector(width/2-side/2, height/2-side/2+i*side/8+side/16), 3*PI/2
+        ));
+    }
+    for (var i = 0; i < 8; i++) {
+        append(p, new Particle(
+            createVector(width/2-side/2+i*side/8+side/16, height/2+side/2-side/8), 0
+        ));
+    }
+    for (var i = 0; i < 8; i++) {
+        append(p, new Particle(
+            createVector(width/2+side/2-side/8, height/2-side/2+i*side/8+side/16), PI/2
+        ));
     }
 }
 
@@ -27,6 +45,9 @@ function draw() {
         p[i].step();
         if (p[i].blocked) p.splice(i, 1);
     }
+    rectMode(CENTER);
+    fill(0);
+    rect(width/2, height/2, side, side);
 }
 
 // particle class
@@ -38,6 +59,7 @@ function Particle(new_location, new_angle) {
 
     // status
     this.blocked = false;
+    this.life = random(1000, 3000);
 
     // one step into future
     this.step = function() {
@@ -46,19 +68,20 @@ function Particle(new_location, new_angle) {
         var old_y = this.location.y;
 
         // move current particle
-        // this.location.x += this.speed * sin(this.angle);
-        // this.location.y += this.speed * cos(this.angle);
+        this.location.x += this.speed * sin(this.angle);
+        this.location.y += this.speed * cos(this.angle);
 
         // move current particle only on the 8 directions
-        var angle_prime = PI / 4 * ceil(4 * this.angle / PI);
-        this.location.x += this.speed * sin(angle_prime);
-        this.location.y += this.speed * cos(angle_prime);
+        // var angle_prime = PI / 4 * ceil(4 * this.angle / PI);
+        // this.location.x += this.speed * sin(angle_prime);
+        // this.location.y += this.speed * cos(angle_prime);
 
         // change angle randomly
-        this.angle += random(-0.1, 0.1);
+        // this.angle += random(-0.01, 0.01);
+        var n = random(200);
+        this.angle += (n > 198) ? PI/4 : (n < 1) ? -PI/4 : 0;
 
         // draw line
-        stroke(255);
         line(old_x, old_y, this.location.x, this.location.y);
 
         // delete itself if it hits window border
@@ -66,7 +89,16 @@ function Particle(new_location, new_angle) {
             this.location.x >= width - 1 ||
             this.location.y < 1 ||
             this.location.y >= height - 1) {
-                this.blocked = true
+                this.blocked = true;
+                return;
+        }
+
+        // counts down life
+        if (this.life > 0) {
+            this.life--;
+        } else {
+            this.blocked = true;
+            return;
         }
     }
 }
