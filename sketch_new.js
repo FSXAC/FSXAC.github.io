1e3f6cc8533c36b2bf08062f17e36057d4b0373e
Muchen He <mansurhe@outlook.com>
2017-03-19 13:38:03 -0700

    fancy landing page

---
+++ sketch_new.js
@@ -0,0 +1,67 @@
+var colors;
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.class("pcanvas");
+    strokeWeight(5);
+
+    // colors
+    colors = [
+        color(255, 0, 0),
+        color(0, 255, 0),
+        color(0, 0, 255)
+    ];
+}
+
+function draw() {
+    blendMode(BLEND);
+    background(255);
+    blendMode(EXCLUSION);
+    noFill();
+    
+    for (var i = 0; i < 3; i++) {
+        stroke(colors[i]);
+        beginShape();
+        for(var w = -20; w < width + 20; w += 5) {
+            var h = 0.8 * height;
+            // h += 200 * sin(w * 0.03 + frameCount * 0.07 + i * TWO_PI / 3) * pow(abs(sin(w * 0.001 + frameCount * 0.02)), 5);
+
+            // amplitude
+            var a = 10;
+
+            // frequency
+            var f = 0.05;
+
+            // phase 
+            var phase = 0.07*frameCount;
+
+            // second wave
+            // frequency
+            var f2 = 0.01;
+
+            // phase
+            var phase2 = 0.02*frameCount;
+
+            // wave offset
+            var phaseOffset = i * TWO_PI/3;
+
+            // noise
+            // noise amplitude
+            var na = 5;
+
+            // noise phase
+            var nphase = 0.01*frameCount;
+
+            var noiseOffset = na*noise(w, nphase)-0.5;
+            h += a*sin((f*w) + phase + phaseOffset) * pow(sin((f2*w) + phase2), 2);
+            h += noiseOffset;
+            curveVertex(w, h);
+        }
+        endShape();
+    }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
