adf45898c99891ee26a275e756ba41a6bc61318a
Muchen He <mansurhe@outlook.com>
2017-07-13 23:55:06 -0700

    Started a new p5 sketch

---
+++ index_sketch-DESKTOP-CU9Q4HD.js
@@ -0,0 +1,43 @@
+var tickerX;
+var news = [
+    "BREAKING NEWS: NEWS BREAKING NOW DECLARED ILLEGAL",
+    // "THIS WEBSITE CANNOT BE ACCESSED ON A TRAMPOLINE",
+    // ""
+];
+
+function setup() {
+    canvas = createCanvas(windowWidth, 100);
+    canvas.class("pcanvas");
+    canvas.parent('canvasContainer');
+
+    noStroke();
+
+    tickerX = width;
+}
+
+function draw() {
+    background(255);
+    // targetX = lerp(targetX, mouseX, 0.1);
+    fill(200, 0, 0);
+    rect(0, 0, width, 40);
+    fill(220, 100, 100);
+    rect(0, 40, width, 30);
+    // ellipse(targetX, height / 2, 10, 10);
+
+    fill(255);
+    textStyle(BOLD);
+    textStyle(ITALIC);
+    textSize(40);
+    text(news[0], tickerX, 60);
+
+    if (tickerX + textWidth(news[0]) > 0) tickerX -= 5;
+    else tickerX = width;
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, 100);
+}
+
+function mousePressed() {
+    createElement("h2", "Random Message?");
+}
--- index_sketch.js
+++ index_sketch.js
@@ -1,39 +1,58 @@
-var tickerX;
-var news = [
-    "BREAKING NEWS: NEWS BREAKING NOW DECLARED ILLEGAL",
-    // "THIS WEBSITE CANNOT BE ACCESSED ON A TRAMPOLINE",
-    // ""
-];
+var gridSize = 20;
 
 function setup() {
-    canvas = createCanvas(windowWidth, 100);
+    // canvas = createCanvas(windowWidth, 100);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
     canvas.class("pcanvas");
-    canvas.parent('canvasContainer');
-
-    noStroke();
-
-    tickerX = width;
+    // canvas.parent('canvasContainer');
 }
 
 function draw() {
-    background(255);
-    // targetX = lerp(targetX, mouseX, 0.1);
-    fill(200, 0, 0);
-    rect(0, 0, width, 40);
-    fill(220, 100, 100);
-    rect(0, 40, width, 30);
-    // ellipse(targetX, height / 2, 10, 10);
+    // background(230);
 
-    fill(255);
-    textStyle(BOLD);
-    textStyle(ITALIC);
-    textSize(40);
-    text(news[0], tickerX, 60);
-
-    if (tickerX + textWidth(news[0]) > 0) tickerX -= 5;
-    else tickerX = width;
+    // draw dots
+    stroke(200);
+    for (var x = 0; x < width/gridSize; x++) {
+        for (var y = 0; y < height/gridSize; y++) {
+            point(x * gridSize, y * gridSize);
+        }
+    }
 }
+
+function mousePressed() {
+    drawChip(mouseX, mouseY, randomInt(10), randomInt(10), "");
+} 
 
 function windowResized() {
     resizeCanvas(windowWidth, 100);
+}
+
+function snap(n) {
+    return (gridSize * round(n / gridSize));
+}
+
+function randomInt(n) {
+    return floor(random(n));
+}
+
+function drawChip(posX, posY, hPins, vPins, partName) {
+    var w, h;
+
+    // console.log(hPins, vPins);
+
+    // if no pins
+    if (hPins == 0 && vPins == 0) {
+        h = w = gridSize / 2;
+        rect(snap(posX) - w/2, snap(posY) - h/2, w, h);
+    } else {
+        w = (hPins == 0) ? 10 : gridSize * hPins;
+        h = (vPins == 0) ? 10 : gridSize * vPins;
+        rect(snap(posX) - (gridSize / 2), snap(posY) - (gridSize / 2), w, h);
+
+        // w = (hPins == 0) ? gridSize / 2 : gridSize * hPins;
+        // h = (vPins == 0) ? gridSize / 2 : gridSize * vPins;
+        console.log(w, h);
+        // rect(snap(posX) - (gridSize / (hPins == 0) ? 4 : 2), snap(posY) - (gridSize / (vPins == 0) ? 4 : 2), w, h);
+    }
 }
