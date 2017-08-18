b9c0c5a2d2b90a8eb76373c90c4d471d862fe213
Muchen He <mansurhe@outlook.com>
2017-08-17 22:25:17 -0700

    Got project timeline working

---
+++ js/timeline.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function($) {
+    var timelineBlocks = $(".timeline-block");
+    var offset = 0.8;
+
+    hideBlocks(timelineBlocks, offset);
+
+    $(window).on('scroll', function() {
+        if (!window.requestAnimationFrame) {
+            setTimeout(function() {
+                showBlocks(timelineBlocks, offset);
+            }, 100);
+        } else {
+            window.requestAnimationFrame(function() {
+                showBlocks(timelineBlocks, offset);
+            });
+        }
+    });
+});
+
+function hideBlocks(blocks, offset) {
+    blocks.each(function() {
+        ($(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && 
+        $(this).find('.timeline-icon, .timeline-body').addClass('is-hidden');
+    });
+}
+
+function showBlocks(blocks, offset) {
+    blocks.each(function(){
+        ($(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && 
+            $(this).find('.timeline-icon').hasClass('is-hidden')) && 
+        $(this).find('.timeline-icon, .timeline-body').removeClass('is-hidden').addClass('bounce-in');
+    });
+}
