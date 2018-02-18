9cc20ab3efa293189114b2d03dea0ba907de3b6b
Muchen He <mansurhe@outlook.com>
2018-02-18 12:30:20 -0800

    Updated nearly all sites to bootstrap 4

--- js/default.js
+++ js/default.js
@@ -8,51 +8,6 @@ function addFooter() {
     $('#main').append(footer);
 }
 
-function addNavbar_BS3(current) {
-    navs = ['Index', 'Blog', 'Documents', 'Gallery', 'Projects', 'Resume', 'Tools'];
-    links = ['/', 'https://www.muchen.ca/blog', '/documents', '/gallery', '/projects', '/resume', '/tools'];
-
-    var navbarBegin = `
-    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" >
-    <div class="container">
-    <div class="navbar-header">
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-collapsable" aria-expanded="false">
-    <span class="sr-only">Toggle Nav</span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="/">Muchen He</a>
-    </div>
-    <div class="collapse navbar-collapse" id="nav-collapsable">
-    <ul class="nav navbar-nav">
-    `;
-
-    var navbarEnd = `
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-    <li role="presentation"><a href="/archive">Old Stuff</a></li>
-    </ul>
-    </div>
-    </div>
-    </nav>
-    `;
-
-    var outHtml = navbarBegin;
-    for (var i = 0, l = navs.length; i < l; i++) {
-        outHtml += '<li role="presentation"';
-        if (current == navs[i]) outHtml += ' class = "active"';
-        outHtml += '><a ';
-        if (current != navs[i]) outHtml += 'href="' + links[i] + '"';
-        outHtml += '>' + navs[i];
-        if (current == navs[i]) outHtml += '<span class="sr-only">(current)</span>';
-        outHtml += '</a></li>';
-    }
-    outHtml += navbarEnd;
-
-    $('body').prepend(outHtml);
-}
-
 function addNavbar(current) {
     var navs = ['About', 'Blog', 'Documents', 'Gallery', 'Projects', 'Resume', 'Toolbox'];
     var links = ['/', '/blog', '/documents', '/gallery', '/projects', '/resume', '/tools'];
