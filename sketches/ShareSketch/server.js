e44247233ba0e050432280bf1bc675ed14c221fb
Muchen He <i@muchen.ca>
2018-09-11 21:59:39 -0600

    Detached sketches

--- sketches/ShareSketch/server.js
+++
@@ -1,9 +0,0 @@
-// import frame work express into 'express'
-var express = require('express');
-var app = express();
-var server = app.listen(3000); // port 3000
-
-// host everything in 'public'
-app.use(express.static('public'));
-
-console.log("Socket Server Initiated");
