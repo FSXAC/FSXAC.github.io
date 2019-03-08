f4f0e7f55e1c0258d361395762a819d199d57904
Muchen He <mansurhe@outlook.com>
2019-03-07 23:22:55 -0800

    Minor changes

--- js/default.js
+++ js/default.js
@@ -1,20 +1,20 @@
 function addFooter(parent) {
-    parent = parent || '#main';
+    parent = parent || 'main';
     const date = new Date();
-    const footer = '<footer><small><p class="mb-0">Copyright © 2014-' + date.getFullYear() + ' Muchen He</p> \
+    var footerNode = document.createElement('footer');
+    footerNode.innerHTML = '<small><p class="mb-0">Copyright © 2014-' + date.getFullYear() + ' Muchen He</p> \
         This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>. \
-        </small> \
-    </footer>';
-
-    $(parent).append(footer);
+        </small>';
+    document.getElementById(parent).appendChild(footerNode);
 }
 
 function addNavbar(current) {
     const navs = ['About', 'Blog', 'Documents', 'Gallery', 'Projects', 'Resume', 'Sketches', 'Toolbox'];
     const links = ['/', '/blog', '/documents', '/gallery', '/projects', '/resume', '/sketches', '/tools'];
 
+    // <nav class="navbar sticky-top navbar-expand-md navbar-light bg-light">
+
     const navbarBegin = `
-<nav class="navbar sticky-top navbar-expand-md navbar-light bg-light">
     <div class="container-fluid">
         <a href="/" class="navbar-brand">Muchen He</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -32,11 +32,11 @@ function addNavbar(current) {
     </ul>
     `;
 
-    const navbarEnd = `
-        </div>
-    </div>
-</nav>
-    `;
+    // const navbarEnd = "</div></div></nav>";
+    const navbarEnd = "</div></div>";
+
+    var navNode = document.createElement("nav");
+    navNode.className += "navbar sticky-top navbar-expand-md navbar-light bg-light"
 
     var outHtml = navbarBegin;
     for (var i = 0, l = navs.length; i < l; i++) {
@@ -50,5 +50,9 @@ function addNavbar(current) {
         outHtml += '</a></li>';
     }
     outHtml += navbarEnd;
-    $('body').prepend(outHtml);
+
+    navNode.innerHTML = outHtml;
+
+    var bodyNode = document.getElementsByTagName('body')[0]
+    bodyNode.insertBefore(navNode, bodyNode.firstChild);
 }
