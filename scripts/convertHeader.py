15fd708dd537f688c54d03b8ccc5c150b2956cc6
Muchen He <mansurhe@outlook.com>
2019-04-02 23:08:49 -0700

    Title conversion

--- scripts/convertHeader.py
+++ scripts/convertHeader.py
@@ -3,6 +3,7 @@ import re
 import os
 
 re_getDate = re.compile(r'<small>.*([0-9]{4}-[0-9]{2}-[0-9]{2}).*</small>')
+re_getTitle = re.compile(r'^#\s([A-Za-z\s]+)$')
 
 parser = argparse.ArgumentParser(description='Process note md files')
 parser.add_argument('path', help="path to the .md files")
@@ -16,26 +17,46 @@ for file in os.listdir(args.path):
     filepath = os.path.join(args.path, file)
     mdlines = list()
     date = ''
+    title = ''
 
     with open(filepath, 'r', encoding='utf-8') as mdfile:
         mdlines = mdfile.readlines()
 
-        for line in mdlines:
-            dateMatches = re_getDate.findall(line)
-            if dateMatches:
-                date = dateMatches[0]
-                break
+        combinedLines = ''.join(mdlines)
 
-    first_opening = True
+        # for line in mdlines:
+        dateMatches = re_getDate.findall(line)
+        titleMatches = re_getTitle(line)
+
+        if dateMatches:
+            assert len(dateMatches) == 1
+            date = dateMatches[0]
+        
+        if titleMatches:
+            assert len(titleMatches) == 1
+            title = titleMatches[0]
+
+
     with open(filepath, 'w', encoding='utf-8') as mdfile:
+
+        front_matter = False
         for line in mdlines:
 
             stripped = line.strip('\n')
-            
-            if stripped == '---' and first_opening:
-                mdfile.write('date: {}\n'.format(date))
-                first_opening = False
+
+            if stripped == '---' and not front_matter:
+                front_matter = True
                 mdfile.write(line)
+                mdfile.write('date: {}\n'.format(date))
+            
+            elif stripped == '---' and front_matter:
+                front_matter = False
+
+            elif stripped.startswith('title: ') and front_matter:
+                mdfile.write('title: {}\n'.format(title))
+
+            elif stripped == '# {}'.format(title):
+                continue
 
             elif stripped == ':floppy_disk: [Go Back](/documents)':
                 continue
